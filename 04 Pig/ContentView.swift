67913f5d06ace914aa0f7e53b6081bfc1bdd6f83
Jonathan Varghese <jonathanvarghese24@gmail.com>
2023-10-27 10:06:55 -0500

    added roll and hold buttons

--- 04 Pig/ContentView.swift
+++ 04 Pig/ContentView.swift
@@ -25,6 +25,19 @@ struct ContentView: View {
                     .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z:0))
                     .padding(50)
                 CustomText(text: "Turn Score: \(turnScore)")
+                
+                HStack{
+                    Button("Roll"){
+                        
+                    }
+                }
+                .buttonStyle(CustomButtonStyle())
+                .padding()
+                Button("Hold"){
+                    
+                }
+                .buttonStyle(CustomButtonStyle())
+                .padding()
                 CustomText(text: "Game Score: \(turnScore)")
 
                 Spacer()
@@ -43,5 +56,16 @@ struct CustomText: View {
     let text: String
     var body: some View{
         Text(text).font(Font.custom("Marker Felt", size: 36))
+    }
+}
+
+struct CustomButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(width: 50)
+            .font(Font.custom("Marker Felt", size: 24))
+            .background(.red).opacity(configuration.isPressed ? 0.0 : 1.0)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
