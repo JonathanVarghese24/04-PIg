22b94e82ca463102486a0b91a3046240142304f6
Jonathan Varghese <jonathanvarghese24@gmail.com>
2023-10-27 09:32:47 -0500

    Initial Commit

---
+++ 04 Pig/_4_PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4_PigApp.swift
+//  04 Pig
+//
+//  Created by Jonathan Varghese on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct _4_PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
